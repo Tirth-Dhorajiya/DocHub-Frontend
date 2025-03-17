36e4f27f85a191ba4f3a9ab1ee538462a8f08694
Tirth-Dhorajiya <dhorajiyatirth3012@gmail.com>
2025-03-17 18:28:00 +0530

    Loader add

--- src/Pages/Bookapp/Bookdetails.jsx
+++ src/Pages/Bookapp/Bookdetails.jsx
@@ -25,63 +25,77 @@ export default function Bookdetails() {
       });
   }, [id]);
 
-  if (loading)
-    return (
-      <BeatLoader
-        color="#6c63ff"
-        className="d-flex justify-content-center align-items-center vh-100"
-      />
-    );
+  // if (loading)
+  //   return (
+  //     <BeatLoader
+  //       color="#6c63ff"
+  //       className="d-flex justify-content-center align-items-center vh-100"
+  //     />
+  //   );
   if (!doctor) return <h2>Doctor not found</h2>;
 
   return (
     <>
       <Header />
-      <Container>
-        <Row className="p-5">
-          <Col
-            lg={6}
-            md={12}
-            sm={12}
-            className="d-flex justify-content-center "
-            style={{ maxHeight: "500px" }}
-          >
-            <img
-              src={doctor.img}
-              alt={doctor.name}
-              className="mw-100"
-              style={{ backgroundColor: "#6c63ff" }}
-            />
-          </Col>
-          <Col
-            style={{ border: "1px solid rgb(160, 143, 143)" }}
-            lg={6}
-            md={12}
-            sm={12}
-            className="p-5"
-          >
-            <h1
-              className="p-1"
-              style={{ color: "rgba(0, 124, 157, 0.9)", fontWeight: "bold" }}
-            >
-              {doctor.name}
-            </h1>
-            <p className="p-1">
-              <strong>Speciality:</strong>
-              {doctor.title}
-            </p>
-            <p className="p-1">
-              <strong>Experience:</strong> {doctor.experience}
-            </p>
-            <strong style={{ padding: "5px" }}>About</strong>
-            <p className="p-1">{doctor.about}</p>
-            <p className="p-1">
-              <strong>Appointment fee:</strong> {doctor.fee}
-            </p>
-          </Col>
-        </Row>
-      </Container>
-      <Slot doctor={doctor} />
+      {loading ? (
+        <div
+          className="d-flex justify-content-center align-items-center"
+          style={{ height: "80vh" }}
+        >
+          <BeatLoader color="#028885" />
+        </div>
+      ) : (
+        <>
+          <Container>
+            <Row className="p-5">
+              <Col
+                lg={6}
+                md={12}
+                sm={12}
+                className="d-flex justify-content-center "
+                style={{ maxHeight: "500px" }}
+              >
+                <img
+                  src={doctor.img}
+                  alt={doctor.name}
+                  className="mw-100"
+                  style={{ backgroundColor: "#6c63ff" }}
+                />
+              </Col>
+              <Col
+                style={{ border: "1px solid rgb(160, 143, 143)" }}
+                lg={6}
+                md={12}
+                sm={12}
+                className="p-5"
+              >
+                <h1
+                  className="p-1"
+                  style={{
+                    color: "rgba(0, 124, 157, 0.9)",
+                    fontWeight: "bold",
+                  }}
+                >
+                  {doctor.name}
+                </h1>
+                <p className="p-1">
+                  <strong>Speciality:</strong>
+                  {doctor.title}
+                </p>
+                <p className="p-1">
+                  <strong>Experience:</strong> {doctor.experience}
+                </p>
+                <strong style={{ padding: "5px" }}>About</strong>
+                <p className="p-1">{doctor.about}</p>
+                <p className="p-1">
+                  <strong>Appointment fee:</strong> {doctor.fee}
+                </p>
+              </Col>
+            </Row>
+          </Container>
+          <Slot doctor={doctor} />
+        </>
+      )}
       <Footer />
     </>
   );
