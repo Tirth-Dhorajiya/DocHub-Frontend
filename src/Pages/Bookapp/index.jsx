6d0f08d212db39058dba489dbfb15a7c31ec1207
Tirth-Dhorajiya <dhorajiyatirth3012@gmail.com>
2025-03-18 14:51:24 +0530

    download component

--- src/Pages/Bookapp/index.jsx
+++ src/Pages/Bookapp/index.jsx
@@ -20,6 +20,7 @@ import Footer from "../../common/Footer/Footer";
 import Header from "../../common/Header/Header";
 import symptomToSpecialty from "./Symptoms";
 import { BeatLoader } from "react-spinners";
+import DownloadSection from "./DownloadSection";
 
 export default function Bookapp() {
   const [searchQuery, setSearchQuery] = useState("");
@@ -579,34 +580,7 @@ export default function Bookapp() {
           </Button>
         </Modal.Footer>
       </Modal>
-      <section className="section-download">
-        <Container>
-          <Row>
-            <Col lg={6} md={12} sm={12}>
-              <div className="dbanner">
-                <img src="images/downloadbanner.png" alt="app_mobile" />
-              </div>
-            </Col>
-            <Col lg={6} md={12} sm={12}>
-              <div className="app-center p-5 text-center">
-                <h2 className="p-3">Download DocHub App</h2>
-                <p>Book appointment & health checkups;</p>
-                <p>Online lab test & consult doctor online</p>
-                <p>Get the link to download the app</p>
-                <div className="download-btn pt-5">
-                  <a
-                    href="https://play.google.com/store/games?hl=en"
-                    target="blank"
-                  >
-                    <img src="images/play-store.png" alt="" className="p-2" />
-                    Play Store
-                  </a>
-                </div>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </section>
+      <DownloadSection />
       <ScrollToTop smooth color="#028885" />
       <Footer />
     </>
